8a6dd1c0a3946137ccf1d8617e350963793bb9d6
jhkang <serdozz@naver.com>
2022-09-28 11:39:48 +0900

    feat: 입력값 검증 기능 구현

--- src/main/java/baseball/view/ConsoleIO.java
+++ src/main/java/baseball/view/ConsoleIO.java
@@ -1,12 +1,18 @@
 package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
+import static baseball.common.Validator.*;
 
 public class ConsoleIO {
     private static final String INPUT_SENTENCE = "숫자를 입력해주세요 : ";
 
     public static String inputAnswer() {
         System.out.print(INPUT_SENTENCE);
+        String input = Console.readLine();
 
-        return Console.readLine();
+        if (!isValidBaseballNumber(input)) {
+            throw new IllegalArgumentException();
+        }
+
+        return input;
     }
 }
